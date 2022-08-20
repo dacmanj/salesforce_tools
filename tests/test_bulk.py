807320a7be231fcdd6a0d8e9a461a49aa7f7fa34
David Manuel <david@dcmanjr.com>
2022-08-20 15:38:00 -0700

    more tests

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -1,3 +1,4 @@
+import pytest
 from arroyo_salesforce.bulk import SalesforceBulkAPI
 from arroyo_salesforce.auth import TOKEN_URL
 from mock_responses import mock_response, API_VERSION
@@ -6,20 +7,35 @@ from mock_responses import mock_response, API_VERSION
 def fake_open(url, new=None):
     pass
 
+@pytest.fixture()
+def fake_auth_code_path(state='12345'):
+    return
 
-def fake_auth_code_path(self, state='12345'):
-    return f"/callback?code=aPrxT9Lzoezd_72OG18vycl0gdfPS8jLc.IfY2H8ana14w0GZuKFcMApsIf6xyjAm3HbskSaHQ%3D%3D&state={state}"
+
+@pytest.fixture
+def mock_func1():
+    def mock_ret(*args, **kwargs):
+        return 2
 
 
+def mock_auth(func):
+    def inner(monkeypatch, requests_mock, *args, **kwargs):
+        state = '12345'
+        auth_code_redirect_url = f"/callback?code=aPrxT9Lzoezd_72OG18vycl0gdfPS8jLc.IfY2H8ana14w0GZuKFcMApsIf6xyjAm3HbskSaHQ%3D%3D&state={state}"
+        requests_mock.post(TOKEN_URL,
+                           text=mock_response('id_token'),
+                           status_code=200)
+        monkeypatch.setattr("webbrowser.open", lambda url, new: True, raising=True)
+        monkeypatch.setattr("arroyo_salesforce.auth.OAuth2Session.new_state", lambda x: state, raising=True)
+        monkeypatch.setattr("arroyo_salesforce.auth.CallbackServer.get_auth", lambda x: auth_code_redirect_url, raising=True)
+        return func(monkeypatch, requests_mock, *args, **kwargs)
+    return inner
+
+
+@mock_auth
 def test_bulk(monkeypatch, requests_mock):
     requests_mock.post(f'/services/async/{API_VERSION}/job',
                        text=mock_response('bulk_api_job_detail'),
                        status_code=200)
-    requests_mock.post(TOKEN_URL,
-                       text=mock_response('id_token'),
-                       status_code=200)
-    monkeypatch.setattr("webbrowser.open", lambda url, new: True, raising=True)
-    monkeypatch.setattr("arroyo_salesforce.auth.OAuth2Session.new_state", lambda x: '12345', raising=True)
-    monkeypatch.setattr("arroyo_salesforce.auth.CallbackServer.get_auth", fake_auth_code_path, raising=True)
     s = SalesforceBulkAPI()
     s.create_job(operation='insert', sf_object='Contact')
