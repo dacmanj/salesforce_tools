a52c2009da4b2e62a1eb99e706dffbda2791945f
David Manuel <david@dcmanjr.com>
2022-08-20 15:38:56 -0700

    cleanup

--- tests/mock_responses.py
+++ tests/mock_responses.py
@@ -1,4 +1,4 @@
-from arroyo_salesforce.util import sf_id_checksum, datetime_as_epoch, datetime_as_str
+from arroyo_salesforce.util import sf_id_checksum, SFDateTime
 import string
 import random
 import json
@@ -7,7 +7,7 @@ import sqlite3
 API_VERSION = 54.0
 
 
-def fake_id(prefix='001', instance='8X', reserved='0', id_size=6):
+def fake_sf_id(prefix='001', instance='8X', reserved='0', id_size=6):
     valid_id_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
     unique_id = ''.join(random.choice(valid_id_chars) for i in range(id_size)).zfill(9)
     sf_id_15_char = f"{prefix}{instance}{reserved}{unique_id}"
